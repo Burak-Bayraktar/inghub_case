149ea20f66d50fd9af396633d2016486db1248d2
Burak Bayraktar <byrktrhmx@gmail.com>
2025-08-30 17:03:12 +0300

    Add local files

---
+++ src/i18n/tr.js
@@ -0,0 +1,31 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's1e45a4d7635fbda7': `Departman`,
+'s2c57913ffd36d298': `Ad`,
+'s2ceb11be2290bb1b': `İptal`,
+'s33f85f24c0f5f008': `Kaydet`,
+'s3741f519c32ac391': `Yeni`,
+'s382a2aa3984474dd': `Oluştur`,
+'s3db6c3999daeecdb': `Çalışan Listesi`,
+'s53802e3f37d072a7': `Doğum Tarihi`,
+'s623dba9a2d66680e': `Düzenleme modundasınız`,
+'s6686fcc6d2c8f0bd': `Not`,
+'s772528836550622c': `İşe Giriş Tarihi`,
+'s8a4ff2bd9c13498c': `Kayıt`,
+'s8af61807443f32a4': `İşlemler`,
+'s95a75a2392af5780': `Soyad`,
+'s99f110d27e30b289': `Başlık`,
+'sc1611c0558728a2a': `Pozisyon`,
+'sd1f44f1a8bc20e67': `E-posta`,
+'sf1bc83d2509bafcf': `Telefon`,
+    };
+  
