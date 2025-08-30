21a11b1013d5394e3a2cb69dec2789b2be09573f
Burak Bayraktar <byrktrhmx@gmail.com>
2025-08-30 17:10:37 +0300

    Add localization to ItemForm component

--- src/components/item-form.js
+++ src/components/item-form.js
@@ -1,6 +1,8 @@
-import { LitElement, html, css } from 'lit';
+import { html, css } from 'lit';
+import { msg } from '@lit/localize';
+import { LocalizedComponent } from './localized-component.js';
 
-export class ItemForm extends LitElement {
+export class ItemForm extends LocalizedComponent {
   static styles = css`
     :host{display:block}
     form{display:grid;gap:12px}
@@ -24,33 +26,34 @@ export class ItemForm extends LitElement {
     const id = this.location?.params?.id;
     if (id){
       this.isEdit = true;
-      this.title = `Kayıt #${id}`;
-      this.note  = 'Düzenleme modundasın';
+      this.title = `${msg('Record')} #${id}`;
+      this.note  = msg('You are in edit mode');
     }
   }
 
   onSubmit(e){
     e.preventDefault();
-    location.hash = '/list';
+    location.hash = '/';
   }
 
   render(){
     return html`
       <form @submit=${this.onSubmit}>
         <label>
-          <span>Başlık</span>
+          <span>${msg('Title')}</span>
           <input type="text" .value=${this.title} @input=${e=>this.title=e.target.value} required>
         </label>
         <label>
-          <span>Not</span>
+          <span>${msg('Note')}</span>
           <input type="text" .value=${this.note} @input=${e=>this.note=e.target.value}>
         </label>
         <div class="actions">
-          <a href="#/list"><button type="button">İptal</button></a>
-          <button class="primary" type="submit">${this.isEdit?'Kaydet':'Oluştur'}</button>
+          <a href="#/"><button type="button">${msg('Cancel')}</button></a>
+          <button class="primary" type="submit">${this.isEdit ? msg('Save') : msg('Create')}</button>
         </div>
       </form>
     `;
   }
 }
+
 customElements.define('item-form', ItemForm);
