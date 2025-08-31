12c387fd4b7400e01bd108e39b3eade52443cbda
Burak Bayraktar <byrktrhmx@gmail.com>
2025-08-31 10:03:12 +0300

    Add new icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,3 +2,5 @@ export { editIcon } from './edit-icon.js';
 export { trashIcon } from './trash-icon.js';
 export { chevronLeftIcon } from './chevron-left-icon.js';
 export { chevronRightIcon } from './chevron-right-icon.js';
+export { listIcon } from './list-icon.js';
+export { gridIcon } from './grid-icon.js';
